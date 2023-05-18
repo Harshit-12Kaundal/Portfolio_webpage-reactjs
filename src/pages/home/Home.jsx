f271c511b3838589ec35706843c0107e485a420b
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2023-05-18 08:34:14 +0530

    updates in project

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './home.css'
+import MainHeader from '../../components/MainHeader'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <MainHeader/>
   )
 }
 
