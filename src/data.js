4a6ac1c8a958eee02c56ff18a7c388c5796140cd
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2023-05-19 08:13:10 +0530

    second update

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {SiOpenaigym} from 'react-icons/si'
 
 
@@ -38,28 +39,28 @@ export const links = [
 export const programs = [
     {
         id: 1,
-        icon: '<SiOpenaigym/>',
+        icon: <SiOpenaigym/>,
         title: "Program One",
         info: "This is the day that the lord has made. We will rejoice!",
         path: "/programs/111"
     },
     {
         id: 2,
-        icon: '<SiOpenaigym/>',
+        icon: <SiOpenaigym/>,
         title: "Program Two",
         info: "This is the day that the lord has made. We will rejoice!",
         path: "/programs/222"
     },
     {
         id: 3,
-        icon: '<SiOpenaigym/>',
+        icon: <SiOpenaigym/>,
         title: "Program Three",
         info: "This is the day that the lord has made. We will rejoice!",
         path: "/programs/333"
     },
     {
         id: 4,
-        icon: '<SiOpenaigym/>',
+        icon: <SiOpenaigym/>,
         title: "Program Four",
         info: "This is the day that the lord has made. We will rejoice!",
         path: "/programs/444"
