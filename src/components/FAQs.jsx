f07d7e48f0d093d15ce9abf704a8a4fbc270463a
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2023-05-23 17:38:26 +0530

    updated project

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import SectionHead from './SectionHead'
 import { FaQuestion } from 'react-icons/fa'
 import { faqs } from '../data'
+import FAQ from './FAQ'
 
 
 const FAQs = () => {
   return (
-    <div>FAQs</div>
+    <section>
+        <div className="continer faqs__container">
+            <SectionHead icon={<FaQuestion/>} title='FAQs'/>
+            <div className='faqs__wrapper'>
+                {
+                    faqs.map(({id,question,answer})=>{
+                        return <FAQ key={id} question={question} answer={answer}/>
+                    })
+                }
+             </div>
+        </div>
+    </section>
   )
 }
 
