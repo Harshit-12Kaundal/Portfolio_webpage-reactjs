2ffa587a2a064fde409ad9447172f9df7139de6b
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2023-05-26 06:34:11 +0530

    adding footer

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,9 +6,30 @@ import Card from '../UI/Card'
 import {IoIosArrowDropleftCircle, IoIosArrowDroprightCircle} from 'react-icons/io'
 import { testimonials } from '../data'
 
+
+
 const Testimonials = () => {
-  const[index,setIndex]=useState(1)
+  const[index,setIndex]=useState(0)
   const {name,quote,job,avatar} = testimonials[index];
+
+
+  const prevTetimonialHander=()=>{
+    setIndex(prev=>prev-1);
+
+    if(index <=0){
+      setIndex(testimonials.length-1);
+    }
+  }
+  const nextTetimonialHander=()=>{
+    setIndex(prev=>prev+1);
+
+    if(index >= testimonials.length-1){
+      setIndex(0);
+    }
+  }
+
+  
+
   return(
       <section className='testimonials'>
         <div className="container testimonials__container">
@@ -22,8 +43,8 @@ const Testimonials = () => {
             <small className='testimonial__title'>{job}</small>
           </Card>
           <div className="testimonials__btn-container">
-            <button className='testimonials__btn'><IoIosArrowDropleftCircle/></button>
-            <button className='testimonials__btn'><IoIosArrowDroprightCircle/></button>
+            <button className='testimonials__btn' onClick={prevTetimonialHander}><IoIosArrowDropleftCircle/></button>
+            <button className='testimonials__btn' onClick={nextTetimonialHander}><IoIosArrowDroprightCircle/></button>
           </div>
         </div>
       </section>
