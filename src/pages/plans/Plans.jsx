28116179271c57ef22f33067b12d81f0e024155f
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2023-06-07 06:49:10 +0530

    plans

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './plans.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_4.jpg'
+import Card from '../../UI/Card'
+import {plans} from '../../data'
 
 const Plans = () => {
   return(
-    <div>Plans</div>
+    <>
+      <Header title="Membeship Plans" image={HeaderImage}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quibusdam nostrum repudiandae necessitatibus error officiis molestiae odio quisquam delectus ut?
+      </Header>
+
+      <section className="plans">
+        <div className="container plans__container">
+          {
+            plans.map(({id,name,desc,price,features})=>{
+              return <Card key={id} className={'plans'}>
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>{`$${price}`}</h1><h2>/mo</h2>
+                <h4>Features</h4>
+                {
+                  features.map(({feature,available},index) =>{
+                    return <p key></p>
+                  })
+                }
+              </Card>
+            })
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
